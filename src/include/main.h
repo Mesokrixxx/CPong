f7f32a1b7290dc4d4850e6f868c3c71d235dc2f8
Mesokrixxx <fldnadam@gmail.com>
2024-10-13 19:17:38 +0200

    Working player

--- src/include/main.h
+++ src/include/main.h
@@ -1,6 +1,31 @@
-#ifndef PONG_MAIN_H
-#define PONG_MAIN_H 
+# ifndef PONG_MAIN_H
+# define PONG_MAIN_H
+# include <stdint.h> 
+#include <stdio.h>
+# include <SDL2/SDL.h>
 
+typedef float    f32;
+typedef double   f64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
 
+typedef enum { false = 0, true = 1 } Bool;
 
-#endif
+typedef struct Vec2_s { f32 x, y; } Vec2;
+typedef struct Vec2i_s { i32 x, y; } Vec2i;
+
+# define ASSERT(_c, ...) if (!(_c)) { fprintf(stderr, __VA_ARGS__); exit(1); }
+
+# define WindowWidth 1280
+# define WindowHeight 720
+
+# define MapWidth 384
+# define MapHeight 216
+
+# endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,86 @@
-#include <stdio.h>
+# include "include/main.h"
+# include "include/player.h"
 
-int main(int argc, char *argv[])
-{
-	printf("Hello world\n");
+struct MAIN_S {
+	SDL_Window* window;
+	SDL_Texture* texture;
+	SDL_Renderer* renderer;
+	u32 pixels[MapWidth * MapHeight];
+
+	Bool quit;
+} state;
+
+static void init() {
+	playerInit();
+}
+
+static void update() {
+	playerUpdate();
+}
+
+static void render() {
+	playerDraw(state.pixels);
+}
+
+int main(int argc, char *argv[]) {
+	ASSERT(
+		!SDL_Init(SDL_INIT_VIDEO),
+		"Failed to initialize SDL: %s\n",
+		SDL_GetError());
+
+	state.window = 
+		SDL_CreateWindow(
+			"C PONG",
+			SDL_WINDOWPOS_CENTERED_DISPLAY(1), SDL_WINDOWPOS_CENTERED_DISPLAY(1),
+			WindowWidth, WindowHeight,
+			SDL_WINDOW_ALLOW_HIGHDPI);
+	ASSERT(state.window, "Failed to create SDL window: %s\n", SDL_GetError());
+
+	state.renderer = SDL_CreateRenderer(state.window, -1, SDL_RENDERER_PRESENTVSYNC);
+	ASSERT(state.renderer, "Failed to create SDL renderer: %s\n", SDL_GetError());
+
+	state.texture = 
+		SDL_CreateTexture(
+			state.renderer,
+			SDL_PIXELFORMAT_ABGR8888,
+			SDL_TEXTUREACCESS_STREAMING,
+			MapWidth, MapHeight);
+	ASSERT(state.texture, "Failed to create SDL texture: %s\n", SDL_GetError());
+
+	init();
+	while (!state.quit) {
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev)) {
+			switch (ev.type) {
+				case SDL_QUIT:
+					state.quit = true;
+					break; 
+			}
+		}
+
+		const u8 *keyState = SDL_GetKeyboardState(NULL);
+
+		if (keyState[SDL_SCANCODE_ESCAPE])
+			state.quit = true;
+
+		memset(state.pixels, 0, sizeof(state.pixels));
+
+		update();
+		render();
+
+		SDL_UpdateTexture(state.texture, NULL, state.pixels, MapWidth * 4);
+		SDL_RenderCopyEx(
+			state.renderer,
+			state.texture,
+			NULL, NULL,
+			0.0,
+			NULL,
+			SDL_FLIP_VERTICAL);
+		SDL_RenderPresent(state.renderer);
+	}
+
+	SDL_DestroyTexture(state.texture);
+	SDL_DestroyRenderer(state.renderer);
+	SDL_DestroyWindow(state.window);
 	return 0;
 }
